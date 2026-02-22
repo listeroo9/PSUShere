fc924d781e1e990b638b6783e7fdd716d3e1c0c0
listeroo9 <listeroo92004@gmail.com>
2026-02-23 05:59:22 +0800

    added requirements

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,9 +1,44 @@
 from django.contrib import admin
-
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+
+# CollegeAdmin
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at")
+    search_fields = ("college_name",)
+    list_filter = ("created_at",)
+
+
+# ProgramAdmin
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("prog_name", "college__college_name")
+    list_filter = ("college",)
+
+
+# OrganizationAdmin
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college", "description")
+    search_fields = ("name", "description")
+    list_filter = ("college",)
+
+
+# StudentAdmin
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname")
+
+
+# OrgMemberAdmin
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization", "date_joined")
+    search_fields = ("student__lastname", "student__firstname")
+
+    def get_member_program(self, obj):
+        return obj.student.program if obj.student else None
+    get_member_program.short_description = "Program"
